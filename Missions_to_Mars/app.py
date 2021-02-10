99ada10d9b69c635f2c5e11a124c0b69d9c1ae6a
Janelle <jmbnw9@gmail.com>
2021-02-10 07:54:24 -0600

    updated my scrape and app files

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -14,21 +14,22 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def home():
 
     # Find one record of data from the mongo database
-    mars__data = mongo.db.collection.find_one()
+    mars_dict = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", mars_data = mars_data)
+    return render_template("index.html", mars_data = mars_dict)
 
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
 
+    mars_dict = mongo.db.collection
     # Run the scrape function
     mars_data = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mars_dict.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -8,18 +8,17 @@ import requests
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
-    return Browser("chrome", **executable_path, headless=False)
+    executable_path = {"executable_path": "C:/Users/Janelle Goddard/Downloads/chromedriver_win32/chromedriver"}
+    return Browser("chrome", **executable_path)
 
+mars_data={}
 
 def scrape_info():
     browser = init_browser()
 
     # Visit mars websites
+
     url = 'https://mars.nasa.gov/news/'
-    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
-    url = 'https://space-facts.com/mars/'
-    hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
 
     time.sleep(1)
@@ -31,21 +30,34 @@ def scrape_info():
     # Scrape the News title & paragraph
     stepone = soup.find('ul', class_='item_list')
 
-
     steptwo = stepone.find('li', class_='slide')
     news_title = steptwo.find('div', class_='content_title').text
     news_p = steptwo.find('div', class_='article_teaser_body').text
 
     # Scrape the featured image
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+
+    browser.visit(url)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+
     feature_image = soup.find('div', class_='floating_text_area').a['href']
     featured_image_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/" + feature_image
 
     # Scrape the mars table
+    url = 'https://space-facts.com/mars/'
+
     mars_table = pd.read_html(url)
     mars_table[0]
     mars_html_table = mars_table[0].to_html()
 
     # Scrape the mars hempispheres
+    hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(hemisphere_url)
+    html_hemisphere = browser.html
+    soup = BeautifulSoup(html_hemisphere, 'html.parser')
     hemispheres = soup.find_all('div', class_='item')
     # Create an empty list to hold all the hempisphere urls
     hemisphere_images = []
@@ -63,7 +75,7 @@ def scrape_info():
             soup.find("img", class_="wide-image")["src"]
     # #Append the dictionary with the image url string and the hemisphere title to a list
         hemisphere_images.append({"title": title, "img_url": img_url})
-
+   
     # Store data in a dictionary
     mars_data = {
         "news_title": news_title,
